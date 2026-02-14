5e64b769811ed4e89ce55eed7e2d880051d6685f
sharexy23 <omotietie@yahoo.com>
2026-02-14 18:17:16 +0100

    first commit

--- src/config/passportHandler.ts
+++ src/config/passportHandler.ts
@@ -1,45 +1,45 @@
-import { AppDataSource } from "@database/data-source";
-import { User } from "@database/entity/User";
-import { IResponseError } from "@utils/responses";
-import passport from "passport";
-import passportJwt from "passport-jwt";
-const userRepository = AppDataSource.getRepository(User);
-const JwtStrategy = passportJwt.Strategy;
-const ExtractJwt = passportJwt.ExtractJwt;
+// import { AppDataSource } from "@database/data-source";
+// import { User } from "@database/entity/User";
+// import { IResponseError } from "@utils/responses";
+// import passport from "passport";
+// import passportJwt from "passport-jwt";
+// const userRepository = AppDataSource.getRepository(User);
+// const JwtStrategy = passportJwt.Strategy;
+// const ExtractJwt = passportJwt.ExtractJwt;
 
-passport.use(
-  new JwtStrategy(
-    {
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
-    },
-    async (verifiedToken, done) => {
-      try {
-        const user = await userRepository
-          .createQueryBuilder()
-          .where(`"id" = :id`, { id: verifiedToken.id })
-          .andWhere(`"email" = :email`, { email: verifiedToken.email })
-          .getOne();
-        let error: IResponseError;
-        if (!user) {
-          error = new Error("Unauthorized");
-          error.status = 401;
-          return done(error);
-        }
-        return done(null, user);
-        // if (user && user.is_suspended) {
-        //   error = new Error("User is suspended");
-        //   error.status = 410;
-        //   return done(error);
-        // } else {
-        //   return done(null, user);
-        // }
-      } catch (error) {
-        return done(error);
-      }
-    },
-  ),
-);
+// passport.use(
+//   new JwtStrategy(
+//     {
+//       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+//       secretOrKey: process.env.JWT_SECRET,
+//     },
+//     async (verifiedToken, done) => {
+//       try {
+//         const user = await userRepository
+//           .createQueryBuilder()
+//           .where(`"id" = :id`, { id: verifiedToken.id })
+//           .andWhere(`"email" = :email`, { email: verifiedToken.email })
+//           .getOne();
+//         let error: IResponseError;
+//         if (!user) {
+//           error = new Error("Unauthorized");
+//           error.status = 401;
+//           return done(error);
+//         }
+//         return done(null, user);
+//         // if (user && user.is_suspended) {
+//         //   error = new Error("User is suspended");
+//         //   error.status = 410;
+//         //   return done(error);
+//         // } else {
+//         //   return done(null, user);
+//         // }
+//       } catch (error) {
+//         return done(error);
+//       }
+//     },
+//   ),
+// );
 
 // passport.use(
 //   new PassportFacebook.Strategy(
@@ -60,17 +60,17 @@ passport.use(
 //   ),
 // );
 
-passport.serializeUser(function (user, cb) {
-  cb(null, user);
-});
+// passport.serializeUser(function (user, cb) {
+//   cb(null, user);
+// });
 
-passport.deserializeUser(function (obj: any, cb) {
-  cb(null, obj);
-});
+// passport.deserializeUser(function (obj: any, cb) {
+//   cb(null, obj);
+// });
 
-export default passport;
+// export default passport;
 
-export const passportMiddleWareOpts = {
-  session: false,
-  failWithError: true,
-};
+// export const passportMiddleWareOpts = {
+//   session: false,
+//   failWithError: true,
+// };
